1efdd9b0c976698b56add998539a86107be740e2
Hannu Siikonen <hannu.siikonen@gmail.com>
2017-01-13 09:07:08 -0600

    Final plotting and peakplotter added

--- fitNcalibrate/peakplotter.py
+++ fitNcalibrate/peakplotter.py
@@ -6,15 +6,15 @@ import tdrstyle
 
 # Add a zero to the theoretical values to make a nice y=x plot
 theor=array('d',[0.0,65.740336799410,67.570939637681,69.390239814814])
-exper=array('d',[64.3,66.05,66.97])
-#exper=array('d',[64.89,66.34,67.09])
+#exper=array('d',[64.3,66.05,66.97]) # Pseudo
+exper=array('d',[64.89,66.34,67.09])
 theor_err=array('d',[0.0079,0.0077,0.0076])
-exper_err=array('d',[0.14,0.14,0.135])
-#exper_err=array('d',[0.59,0.26,0.64])
+#exper_err=array('d',[0.14,0.14,0.135]) # Pseudo
+exper_err=array('d',[0.59,0.26,0.64])
 
 #for i in range(0,len(exper)):
 #    exper[i]=(exper[i]-16.51)/0.729
-#    exper_err[i]=(exper_err[i]-16.52)/0.729
+#    exper_err[i]=(exper_err[i])/0.729
 
 c1 = TCanvas("canvas 1", "c1", 800, 600)
 c1.cd()
@@ -50,7 +50,7 @@ label2.SetTextSize(0.06)
 label2.SetTextAlign(11)
 label2.DrawLatex(0.33, 0.92, "#it{Simulation}")
 
-leg = TLegend(0.1,0.75,0.4,0.9)
+leg = TLegend(0.1,0.78,0.38,0.9)
 leg.AddEntry(graph,"Simulation","p")
 leg.AddEntry(ideal,"Expectation","l")
 leg.AddEntry(gfit,"Fit","l")
